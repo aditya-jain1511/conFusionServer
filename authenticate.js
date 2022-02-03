c5489031e0fbfc632d772461da91001790d790b5
Aditya Jain <aditya151101@gmail.com>
2022-02-04 01:52:33 +0530

    assignment 3

--- authenticate.js
+++ authenticate.js
@@ -8,7 +8,7 @@ var jwt =require('jsonwebtoken'); //used to create, sign and verify token
 
 var config = require('./config.js');
 
-passport.use(new LocalStrategy(User.authenticate()));
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -40,3 +40,14 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done)=>{
 }));
 
 exports.verifyUser = passport.authenticate('jwt', {session : false});
+
+exports.verifyAdmin = function (req, res, next){
+    if(req.user.admin){
+        next()
+    }
+    else{
+        err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+}
