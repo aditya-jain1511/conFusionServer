fbe0003ec6575a5ee4e51866fb4b5d150e03f474
Aditya Jain <aditya151101@gmail.com>
2022-02-12 20:56:23 +0530

    facebook passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/confusion'
+    'mongoUrl': 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientId: '3030426077170013',
+        clientSecret: '990aab26cad4d76eef632d8ab487e211' 
+    }
 }
